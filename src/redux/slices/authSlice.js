6aac718ed4efe964fd7222d8b1347f82851c0503
gabtoledano <gabrieltoledano19@gmail.com>
2026-03-30 14:55:33 +0200

    Setup architecture React + Redux store + authSlice

---
+++ src/redux/slices/authSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+  isAuthenticated: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
+      state.error = null;
+    },
+    logoutSuccess: (state) => {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { loginSuccess, logoutSuccess, setUser, setError } =
+  authSlice.actions;
+export default authSlice.reducer;
