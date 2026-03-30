18e8b533e06d2aab31d6d17b218700173b1a7ad5
gabtoledano <gabrieltoledano19@gmail.com>
2026-03-30 15:26:59 +0200

    Ajout Header, Footer et page Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<div>Login</div>} />
         <Route path="/profile" element={<div>Profile</div>} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
