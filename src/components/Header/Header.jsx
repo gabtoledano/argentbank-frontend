ce7bddbdd6a0d38ee84fb0b0cfa3ada8ff914487
gabtoledano <gabrieltoledano19@gmail.com>
2026-04-27 12:46:12 +0200

    Ajout logout + style bouton Sign Out

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutSuccess } from "../../redux/slices/authSlice";
 import logo from "../../assets/img/argentBankLogo.png";
 
 function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logoutSuccess());
+    navigate("/login");
+  };
+
   return (
     <nav className="main-nav">
       <Link className="main-nav-logo" to="/">
@@ -13,10 +24,23 @@ function Header() {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        <Link className="main-nav-item" to="/login">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </Link>
+        {isAuthenticated ? (
+          <>
+            <span className="main-nav-item">
+              <i className="fa fa-user-circle"></i>
+              {user?.userName}
+            </span>
+            <button className="main-nav-item" onClick={handleLogout}>
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </button>
+          </>
+        ) : (
+          <Link className="main-nav-item" to="/login">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </Link>
+        )}
       </div>
     </nav>
   );
