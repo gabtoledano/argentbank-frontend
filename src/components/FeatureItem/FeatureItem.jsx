ccb39cc7a94f357cf4082abdb725e4d08364c834
gabtoledano <gabrieltoledano19@gmail.com>
2026-05-04 16:05:46 +0200

    Ajout composant FeatureItem réutilisable sur page Home

---
+++ src/components/FeatureItem/FeatureItem.jsx
@@ -0,0 +1,11 @@
+function FeatureItem({ icon, alt, title, description }) {
+  return (
+    <div className="feature-item">
+      <img src={icon} alt={alt} className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default FeatureItem;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,31 @@
+import FeatureItem from "../../components/FeatureItem/FeatureItem";
 import iconChat from "../../assets/img/icon-chat.png";
 import iconMoney from "../../assets/img/icon-money.png";
 import iconSecurity from "../../assets/img/icon-security.png";
+
+const features = [
+  {
+    icon: iconChat,
+    alt: "Chat Icon",
+    title: "You are our #1 priority",
+    description:
+      "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+  },
+  {
+    icon: iconMoney,
+    alt: "Money Icon",
+    title: "More savings means higher rates",
+    description:
+      "The more you save with us, the higher your interest rate will be!",
+  },
+  {
+    icon: iconSecurity,
+    alt: "Security Icon",
+    title: "Security you can trust",
+    description:
+      "We use top of the line encryption to make sure your data and money is always safe.",
+  },
+];
 
 function Home() {
   return (
@@ -16,35 +41,15 @@ function Home() {
       </div>
       <section className="features">
         <h2 className="sr-only">Features</h2>
-        <div className="feature-item">
-          <img src={iconChat} alt="Chat Icon" className="feature-icon" />
-          <h3 className="feature-item-title">You are our #1 priority</h3>
-          <p>
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </p>
-        </div>
-        <div className="feature-item">
-          <img src={iconMoney} alt="Money Icon" className="feature-icon" />
-          <h3 className="feature-item-title">
-            More savings means higher rates
-          </h3>
-          <p>
-            The more you save with us, the higher your interest rate will be!
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src={iconSecurity}
-            alt="Security Icon"
-            className="feature-icon"
+        {features.map((feature, index) => (
+          <FeatureItem
+            key={index}
+            icon={feature.icon}
+            alt={feature.alt}
+            title={feature.title}
+            description={feature.description}
           />
-          <h3 className="feature-item-title">Security you can trust</h3>
-          <p>
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </p>
-        </div>
+        ))}
       </section>
     </main>
   );
